9ac9c4f8b52b3fa8e6e7db079e53b26c00b96e13
abelbour <abelbour@users.noreply.github.com>
2016-08-16 15:16:23 -0300

    configurar para co norte

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 ﻿(function (window) {
     window.__env = window.__env || {};
-    window.__env.videoIdSemana = 'x4a2fbi';
-    window.__env.videoIdFinde = 'x4jw7bs';
+    window.__env.videoIdSemana = 'x4ozkh3';
+    window.__env.videoIdFinde = 'x4ozlcm';
     window.__env.videoUrlDigestor1 = '//www.dailymotion.com/embed/video/';
     window.__env.videoUrlDigestor2 = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=1&chromeless=0&theme=dark';
     window.__env.videoUrlDigestor2Flash = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=0&chromeless=0&theme=dark';
     window.__env.videoUrlStream = '//www.dailymotion.com/embed/video/';
-    window.__env.nombreCongregacion = 'Co. Churruarín';
+    window.__env.nombreCongregacion = 'Co. Norte';
     window.__env.programaIdSheets = '1BzuB98iqVhP6h2hr6RJELaRFOc9O2A5bj74mvQyapSQ';
     window.__env.programaUrl1 = '//docs.google.com/spreadsheets/d/';
     window.__env.programaUrl2 = '/pubhtml'; //?widget=true&headers=false&chrome=false';
@@ -15,12 +15,12 @@
     window.__env.salidasUrl1 = '//docs.google.com/spreadsheets/d/';
     window.__env.salidasUrl2 = '/pubhtml?gid=0&single=true&widget=false&headers=false&chrome=false';
     //switches para activar los menus
-    window.__env.mnuPredicacion = true;
-    window.__env.mnuSalidas = true;
-    window.__env.mnuTerritorios = true;
-    window.__env.mnuInforme = true;
+    window.__env.mnuPredicacion = false;
+    window.__env.mnuSalidas = false;
+    window.__env.mnuTerritorios = false;
+    window.__env.mnuInforme = false;
     window.__env.mnuReuniones = true;
-    window.__env.mnuPrograma = true;
+    window.__env.mnuPrograma = false;
     window.__env.mnuEmision = true;
     window.__env.paginaInicio = 'inicio.html';
     // API url
